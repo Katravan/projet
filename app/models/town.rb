f27a85ef9eb49719047149db118c37b9a58d590a
florian.inard <florian.inard@live.fr>
2017-03-25 06:37:14 -0400

    recherche lat lon

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,2 +1,13 @@
 class Town < ActiveRecord::Base
+ before_validation :geocode
+  
+  private
+  def geocode
+    if attribute_present?("name")
+      places = Nominatim.search(self.name).limit(1)
+      place = places.first
+      self.latitude = place.lat
+      self.longitude = place.lon
+    end
+  end 
 end
