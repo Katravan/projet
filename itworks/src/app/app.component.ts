cc9c3d2850985e35aa8c58f647db392daf226eeb
florian.inard <florian.inard@live.fr>
2017-04-20 10:52:03 -0400

    ALLELUIA

--- itworks/src/app/app.component.ts
+++ itworks/src/app/app.component.ts
@@ -9,19 +9,24 @@ import { HeroService } from './hero.service';
   providers: [HeroService]
 })
 
-export class AppComponent implements OnInit {
-  title = 'Tour of Heroes';
+export class AppComponent implements OnInit {  
+  title = 'Tour of Students';
   heroes: Hero[];
-  selectedHero: Hero;
+  selectedHero: Hero;  
+  
   constructor(private heroService: HeroService) { }
+  
   getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+    this.heroService
+        .getHeroes()
+        .then(heroes => this.heroes = heroes );
   }
+  
   ngOnInit(): void {
     this.getHeroes();
   }
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 }
-
--- itworks/src/app/app.module.ts
+++ itworks/src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
+@NgModule({  
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
